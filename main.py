f507de9d88785088c443f9aebcfa43caf32e294d
Ryheeme Donegan <95510965+rylimitless@users.noreply.github.com>
2023-11-25 21:40:43 -0500

    Update main.py
    s

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import JSONResponse
+from typing import Optional
+import os
 
 app = FastAPI()
+
 count = 0
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello Ry"}
+
+@app.get("/count")
+async def counter():
+    global count
+    count += 1
+    return {"count": count}
+
+@app.get("/getcount")
+async def getcounter():
+    return {"count": count}
 
 @app.get("/")
 async def root():
-    global count
-    count +=1
-    return {"count":f"{count}"}
+    return {"message": "Hello World"}
 
-@app.get("/count")
-async def c():
-    global count
-    return {"count value" : f"{count}"}
+@app.post("/api/report")
+async def receive_data(request: Request, screenshot1: Optional[UploadFile] = File(...), screenshot2: Optional[UploadFile] = File(...)):
+    form = await request.form()
+    issue = form.get('issue')
+
+    # Save the files or process the data as needed
+    print(issue)
+
+    with open(os.path.join("uploads", screenshot1.filename), "wb") as buffer:
+        content = await screenshot1.read()  # async read
+        buffer.write(content)
+    
+    with open(os.path.join("uploads", screenshot2.filename), "wb") as buffer:
+        content = await screenshot2.read()  # async read
+        buffer.write(content)
+
+    return JSONResponse(content={"status": "success"}, status_code=200)
+
+# To run the application use command: uvicorn main:app --reload
+
+if "__main__" == __name__:
+    print("Starting API server...")
+    uvicorn.run(app)
